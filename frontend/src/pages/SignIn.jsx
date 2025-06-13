5e09ebe8226be046d0c600e17166f94d034522aa
tartartar <112954170+itsChewin@users.noreply.github.com>
2025-06-13 17:37:35 +0700

    create show/hide password

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function SignIn() {
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <div className="min-h-screen w-full bg-grayBg flex items-center justify-center">
       <div className="bg-white shadow-lg rounded-xl p-8 w-full max-w-md">
@@ -13,11 +15,22 @@ export default function SignIn() {
             placeholder="Username"
             className="w-full mb-4 px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-orange placeholder-gray-400"
           />
+
+          <div className="relative mb-4">
           <input
-            type="password"
+            type={showPassword ? "text" : "password"}
             placeholder="Password"
             className="w-full mb-4 px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-orange placeholder-gray-400"
           />
+            <button
+              type="button"
+              onClick={() => setShowPassword((prev) => !prev)}
+              className="absolute right-3 top-1/3 -translate-y-1/2 text-sm text-gray-600 hover:text-black"
+            >
+              {showPassword ? "Hide" : "Show"}
+            </button>
+          </div>
+
           <div className="flex justify-center mt-1">
             <Link
               to="/calculator"
