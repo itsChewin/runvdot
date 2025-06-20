c62b288c5c14a2aa94cc3c2c0e336bda94ceab59
tartartar <112954170+itsChewin@users.noreply.github.com>
2025-06-20 14:15:32 +0700

    fix  cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -2,8 +2,11 @@
 
 return [
 
-'paths' => ['api/*'],
-'allowed_origins' => ['http://localhost:5173'],
+'paths' => ['api/*', 'sanctum/csrf-cookie'],
+'allowed_origins' => [
+    'http://localhost:5173',
+    'https://runvdot-hzgj4w7gp-tttars-projects.vercel.app',
+],
 'allowed_methods' => ['*'],
 'allowed_headers' => ['*'],
 ];
