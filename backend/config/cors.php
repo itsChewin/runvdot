7efc7e23e87121b7a773d3e9b72c67eca80eaf26
tartartar <112954170+itsChewin@users.noreply.github.com>
2025-06-22 16:53:47 +0700

    modify cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,8 +6,10 @@ return [
 'allowed_origins' => [
     'http://localhost:5173',
     'https://runvdot.vercel.app',
-    'https://runvdot-879oygdt7-tttars-projects.vercel.app',
 ],
+    'allowed_origins_patterns' => [
+        '^https:\/\/runvdot-[a-z0-9]+-tttars-projects\.vercel\.app$',
+    ],
 'allowed_methods' => ['*'],
 'allowed_headers' => ['*'],
 'supports_credentials' => true,
